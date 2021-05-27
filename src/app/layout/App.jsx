96e939df7e08840f5b7482e03e7e0024dbb36444
Hila Kershenovich <hilawol@gmail.com>
2021-05-27 18:29:26 +0300

    first commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line quotes
+import React from "react";
+
+function App() {
+  return (
+    // eslint-disable-next-line jsx-quotes
+    <div className='App'>
+      <h1>Re-vents</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/style.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
+
+function render() {
+  // eslint-disable-next-line react/jsx-filename-extension
+  ReactDOM.render(<App />, rootEl);
+}
+
+// Hot Module Replacement (HMR) exchanges, adds, or removes modules while an application
+// is running, without a full reload. This can significantly speed up development in a few ways:
+
+// Retain application state which is lost during a full reload.
+// Save valuable development time by only updating what's changed.
+// Instantly update the browser when modifications are made to CSS/JS in the source code,
+// which is almost comparable to changing styles directly in the browser's dev tools.
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
